c43d532814839fe2b80b702a152cd96dd2881c7b
Rick Anderson <riande@microsoft.com>
2020-09-28 17:53:46 -1000

    CU50

--- cu50/StartupSQLite.cs
+++ cu50/StartupSQLite.cs
@@ -1,33 +1,34 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using ContosoUniversity.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 namespace ContosoUniversity
 {
-    public class Startup
+    public class StartupSQLite
     {
-        public Startup(IConfiguration configuration)
+        public StartupSQLite(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
+        #region snippet_ConfigureServices
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
 
+            services.AddDbContext<SchoolContext>(options =>
+               options.UseSqlite(Configuration.GetConnectionString("SchoolContext")));
+
             services.AddDatabaseDeveloperPageExceptionFilter();
         }
+        #endregion
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -37,7 +38,6 @@ namespace ContosoUniversity
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
